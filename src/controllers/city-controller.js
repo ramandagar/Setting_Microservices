9671cb8b682b65dbd5883009ab115913da84926f
Raman765 <raman.d@cwstechnology.com>
2024-03-29 12:28:22 +0530

    added controller to services

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,10 +1,10 @@
-const { CityService, CityService } = require('../services/index')
+const { CityService } = require('../services/index')
 
-const CityService = new CityService();
+const cityService = new CityService();
 
 const create = async (req, res) => {
     try {
-        const city = await CityService.createCity(req.body)
+        const city = await cityService.createCity(req.body)
         return res.status(201).json({
             data:city,
             success:true,
@@ -24,26 +24,64 @@ const create = async (req, res) => {
 }
 
 const destroy = async (req, res) => {
-
     try {
-        
+        const response = await cityService.deleteCity(req.params.id)
+        return res.status(201).json({
+            data:response,
+            success:true,
+            message:'succesfully deleted a city',
+            err:{}
+        });
     } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to delete a city',
+            err:error
+        })
         
     }
 }
 
 const update = async (req, res) => {
     try {
-        
+        const city = await cityService.updateCity(req.params.id,req.body)
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:'succesfully fetched a city',
+            err:{}
+        });
     } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to get a city',
+            err:error
+        })
         
     }
 }
 
 const get = async (req, res) => {
     try {
-        
+        const city = await cityService.getCity(req.params.id)
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:'succesfully fetched a city',
+            err:{}
+        });
     } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to get a city',
+            err:error
+        })
         
     }
 }
